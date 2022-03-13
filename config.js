15b701ea5b0a212c59a1443007db12d8243047a7
Vincent Michael <101508608+ma200415@users.noreply.github.com>
2022-03-13 17:29:07 +0000

    ignore DB config file

--- config.js
+++
@@ -1,6 +0,0 @@
-exports.configMongo = {
-  user: "admin" /* the user name */,
-  pwd: "200415" /* the password */,
-  host: "cluster0.dav2k.mongodb.net" /* MongoDB hosting */,
-  dbname: "web-project" /* database name */
-}
