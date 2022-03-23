badf81b6afccedf6f157e8560b5247298c8a9161
Vincent Michael <ma20200415@gmail.com>
2022-03-23 17:35:24 +0800

    add DB InsertOne; password handling

--- helpers/mongodb.js
+++ helpers/mongodb.js
@@ -9,14 +9,36 @@ const DATABASE_NAME = mongoAuth.configMongo.dbname
 
 const CONNECTION_URI = `mongodb+srv://${mongo_username}:${mongo_password}@${mongo_host}/${DATABASE_NAME}?retryWrites=true&w=majority`
 
-exports.run_query = async function run_query(collection, query) {
-  const client = new MongoClient(CONNECTION_URI, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+const client = new MongoClient(CONNECTION_URI, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+exports.query = async function (collection, query) {
   const dbClient = await client.connect();
 
-  const result = await dbClient.db(DATABASE_NAME).collection(collection).find(query).toArray();
-
-  client.close();
-
-  return result;
+  return await dbClient
+    .db(DATABASE_NAME)
+    .collection(collection)
+    .find(query)
+    .toArray();
 }
+
+exports.insertOne = async function (collection, data) {
+  const dbClient = await client.connect();
+
+  return await dbClient
+    .db(DATABASE_NAME)
+    .collection(collection)
+    .insertOne(data)
+    .then(result => ({ status: true, result: result }))
+    .catch(err => ({ status: false, message: err }))
+}
+
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+
+exports.hash = async (password) => {
+  return await bcrypt.hash(password, saltRounds).then((hash) => hash);
+}
+
+exports.comparePassword = async (password, hashedPassword) => {
+  return await bcrypt.compare(password, hashedPassword);
+} 
