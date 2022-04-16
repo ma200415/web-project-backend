7701e83ab158f98d22b8df2637e58d284a732c69
Vincent Michael <ma20200415@gmail.com>
2022-04-17 02:29:33 +0800

    upload db config

---
+++ config.js
@@ -0,0 +1,6 @@
+exports.configMongo = {
+  user: "admin" /* the user name */,
+  pwd: "200415" /* the password */,
+  host: "cluster0.dav2k.mongodb.net" /* MongoDB hosting */,
+  dbname: "web-project" /* database name */
+}
